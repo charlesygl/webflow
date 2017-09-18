a4ab9c7bdb50377d21be63f2e01e0aa54ad2e644
wsimiao <charlesygl@gmail.com>
2017-09-17 22:03:45 -0400

    Completed Modal Overlay

---
+++ app/assets/scripts/modules/Modal.js
@@ -0,0 +1,42 @@
+import $ from 'jquery';
+
+class Modal {
+    
+    constructor() {
+        this.openModalButton = $(".open-modal");
+        this.modal = $(".modal");
+        this.closeModalButton = $(".modal__close");
+        this.events();
+    }
+    
+    events() {
+        
+        // clicking the open button
+        this.openModalButton.click(this.openModal.bind(this));
+        
+        // clicking the close button
+        this.closeModalButton.click(this.closeModal.bind(this));
+        
+        // pushes the any key to close
+        $(document).keyup(this.keyPressHandler.bind(this));
+    }
+    
+    openModal() {
+        this.modal.addClass("modal--is-visible");
+        // return false will prevent auto scrolling up by clicking <a>
+        return false;
+    }
+    
+    closeModal() {
+        this.modal.removeClass("modal--is-visible");
+    }
+    
+    keyPressHandler(e) {
+        if (e.keyCode == 27) {
+            this.closeModal();
+        }
+    }
+    
+}
+
+export default Modal;
